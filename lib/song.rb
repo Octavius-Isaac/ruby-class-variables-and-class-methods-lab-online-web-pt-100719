e13bbb3d8eb70abd3ea4571721b780978f78a0b1
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-11-09 23:07:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,42 +1,40 @@
-class Song 
+class Song
+
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@artists = []
+    @@genres = []
   
-  attr_accessor :name, :artist, :genre 
+    def initialize(song_name,artist,genre)
+      @name = song_name
+      @artist = artist
+      @genre = genre
+      @@count += 1
+      @@artists << artist
+      @@genres << genre
+    end
   
-  @@count = 0 
-  @@artist = []
-  @@genre = [] 
+    def self.count 
+      @@count
+    end
   
-  def initialize(name, artist, genre)
-    @@count += 1 
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@artist << artist
-    @@genres << genre
-  end 
+    def self.artists
+      @@artists.uniq
+    end
   
-   def self.count
-    @@count 
-   end 
+    def self.genres
+      @@genres.uniq
+    end
   
-  def self.artists
-    @@artist.uniq
-    end 
- 
-  def self.genres
-    @@genres.uniq
-  end
- 
-  def self.genre_count
-    genre_count = Hash.new(0)
-    @@genres.each {|genre| genre_count[genre] += 1}
-    genre_count
-  end
+    def self.genre_count
+      genre_count = Hash.new(0)
+      @@genres.each {|genre| genre_count[genre] += 1}
+      genre_count
+    end
   
-  def self.artist_count
-    artist_count = Hash.new(0)
-    @@artist.each {|artist| artist_count[artist] += 1}
-    artist_count
-  end
-  
+    def self.artist_count
+      artist_count = Hash.new(0)
+      @@artists.each {|artist| artist_count[artist] += 1}
+      artist_count
+    end
 end
